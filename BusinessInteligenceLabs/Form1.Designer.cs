42e6788c6d1a77f3b87bab213c9be4f1986565a0
Spell2az <martin_kovac@outlook.com>
2019-03-19 17:09:44 +0000

    Initial commit

--- BusinessInteligenceLabs/Form1.Designer.cs
+++ BusinessInteligenceLabs/Form1.Designer.cs
@@ -29,12 +29,69 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnGetDates = new System.Windows.Forms.Button();
+            this.lstDates = new System.Windows.Forms.ListBox();
+            this.data_set_1DataSet = new BusinessInteligenceLabs.Data_set_1DataSet();
+            this.sheet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sheet1TableAdapter = new BusinessInteligenceLabs.Data_set_1DataSetTableAdapters.Sheet1TableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.data_set_1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sheet1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetDates
+            // 
+            this.btnGetDates.Location = new System.Drawing.Point(198, 79);
+            this.btnGetDates.Name = "btnGetDates";
+            this.btnGetDates.Size = new System.Drawing.Size(75, 23);
+            this.btnGetDates.TabIndex = 0;
+            this.btnGetDates.Text = "GetDates";
+            this.btnGetDates.UseVisualStyleBackColor = true;
+            this.btnGetDates.Click += new System.EventHandler(this.btnGetDates_ClickAsync);
+            // 
+            // lstDates
+            // 
+            this.lstDates.DataSource = this.sheet1BindingSource;
+            this.lstDates.DisplayMember = "Order Date";
+            this.lstDates.FormattingEnabled = true;
+            this.lstDates.Location = new System.Drawing.Point(198, 135);
+            this.lstDates.Name = "lstDates";
+            this.lstDates.Size = new System.Drawing.Size(192, 277);
+            this.lstDates.TabIndex = 1;
+            // 
+            // data_set_1DataSet
+            // 
+            this.data_set_1DataSet.DataSetName = "Data_set_1DataSet";
+            this.data_set_1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sheet1BindingSource
+            // 
+            this.sheet1BindingSource.DataMember = "Sheet1";
+            this.sheet1BindingSource.DataSource = this.data_set_1DataSet;
+            // 
+            // sheet1TableAdapter
+            // 
+            this.sheet1TableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstDates);
+            this.Controls.Add(this.btnGetDates);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGetDates;
+        private System.Windows.Forms.ListBox lstDates;
+        private Data_set_1DataSet data_set_1DataSet;
+        private System.Windows.Forms.BindingSource sheet1BindingSource;
+        private Data_set_1DataSetTableAdapters.Sheet1TableAdapter sheet1TableAdapter;
     }
 }
 
--- BusinessInteligenceLabs/Form1.cs
+++ BusinessInteligenceLabs/Form1.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,85 @@ namespace BusinessInteligenceLabs
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private async void btnGetDates_ClickAsync(object sender, EventArgs e)
+        {
+            var dates = new List<DateTime>();
+            lstDates.DataSource = null;
+            lstDates.Items.Clear();
+
+            var connectionString = Properties.Settings.Default.Data_set_1ConnectionString;
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                var getDates = new OleDbCommand(@"SELECT [Order Date], [Ship Date]
+                                                FROM Sheet1", connection);
+
+                var reader = await getDates.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                   dates.Add(reader.GetDateTime(0));
+                   dates.Add(reader.GetDateTime(1));
+                }
+            }
+
+            lstDates.DataSource = dates.Select(d => d.Date).ToList();
+        }
+
+
+        private async void insertTimeDimension(IEnumerable<DateTime> dates)
+        {
+            var connectionString = Properties.Settings.Default.DestinationDatabaseConnectionString;
+            var insertCommand = new SqlCommand(@"INSERT INTO Time (dayName, dayNumber, monthName, monthNumber, weekNumber, year, weekend, date, dayOfYear)
+                                                    VALUES (@dayName, @dayNumber, @monthName, @monthNumber, @weekNumber, @year, @weekend, @date, @dayOfYear)");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                CheckIfDateIsPresentInDatabase(connection, new DateTime());
+              }
+
+          
+        }
+
+        private bool CheckIfDateIsPresentInDatabase(SqlConnection connection, DateTime date)
+        {
+            bool isPresent;
+
+            var command = new SqlCommand("SELECT id from Time where date = @date", connection);
+            command.Parameters.Add(new SqlParameter("@date", date));
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                isPresent = reader.HasRows;
+            }
+
+            return isPresent;
+        }
+
+        private void InsertDate(SqlConnection connection, DateTime date)
+        {
+            var insertCommand = new SqlCommand(@"INSERT INTO Time (dayName, dayNumber, monthName, monthNumber, weekNumber, year, weekend, date, dayOfYear)
+                                                    VALUES (@dayName, @dayNumber, @monthName, @monthNumber, @weekNumber, @year, @weekend, @date, @dayOfYear)");
+
+            SqlParameter[] parameterList = 
+            {
+                new SqlParameter("@dayName", date.DayOfWeek),
+                new SqlParameter("@dayNumber", date.DayOfWeek),
+                new SqlParameter("@monthName", date.DayOfWeek),
+                new SqlParameter("@monthNumber", date.DayOfWeek),
+                new SqlParameter("@weekNumber", date.DayOfWeek),
+                new SqlParameter("@year", date.DayOfWeek),
+                new SqlParameter("@weekend", IsWeekend(date)),
+                new SqlParameter("@date", date),
+                new SqlParameter("@dayOfYear", date.DayOfYear)
+            };
+            insertCommand.Parameters.AddRange(parameterList);
+
+
+            bool IsWeekend(DateTime dateTime) => dateTime.ToString("dddd") == "Sunday" || dateTime.ToString("dddd") == "Sunday";
         }
     }
 }
