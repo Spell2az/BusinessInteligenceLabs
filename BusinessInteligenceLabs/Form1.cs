7996abed12c6d3e39b0abe6c8705257421d4245a
Spell2az <martin_kovac@outlook.com>
2019-03-19 18:35:54 +0000

    Created time dimension

--- BusinessInteligenceLabs/Form1.cs
+++ BusinessInteligenceLabs/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.OleDb;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,16 @@ namespace BusinessInteligenceLabs
 {
     public partial class Form1 : Form
     {
+
+        #region Get Connection strings
+        private string GetDestinationDbConnectionString =>
+            Properties.Settings.Default.DestinationDatabaseConnectionString;
+
+        private string GetDataSet1DbConnectionString =>
+            Properties.Settings.Default.Data_set_1ConnectionString;
+
+        #endregion
+        bool IsWeekend(DateTime date) => date.ToString("dddd") == "Sunday" || date.ToString("dddd") == "Sunday";
         public Form1()
         {
             InitializeComponent();
@@ -26,9 +37,7 @@ namespace BusinessInteligenceLabs
             lstDates.DataSource = null;
             lstDates.Items.Clear();
 
-            var connectionString = Properties.Settings.Default.Data_set_1ConnectionString;
-
-            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            using (OleDbConnection connection = new OleDbConnection(GetDataSet1DbConnectionString))
             {
                 connection.Open();
                 var getDates = new OleDbCommand(@"SELECT [Order Date], [Ship Date]
@@ -43,24 +52,33 @@ namespace BusinessInteligenceLabs
             }
 
             lstDates.DataSource = dates.Select(d => d.Date).ToList();
+            Debug.WriteLine("Unique Dates in a list "+ dates.Distinct().Count());
+            InsertTimeDimensionRecords(dates);
         }
 
 
-        private async void insertTimeDimension(IEnumerable<DateTime> dates)
+        private void InsertTimeDimensionRecords(IEnumerable<DateTime> dates)
         {
             var connectionString = Properties.Settings.Default.DestinationDatabaseConnectionString;
-            var insertCommand = new SqlCommand(@"INSERT INTO Time (dayName, dayNumber, monthName, monthNumber, weekNumber, year, weekend, date, dayOfYear)
-                                                    VALUES (@dayName, @dayNumber, @monthName, @monthNumber, @weekNumber, @year, @weekend, @date, @dayOfYear)");
+           
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                CheckIfDateIsPresentInDatabase(connection, new DateTime());
+
+                foreach (var date in dates)
+                {
+                    if (!IsDateIsPresentInDatabase(connection, date))
+                    {
+                        InsertDate(connection, date);
+                    }
+                }
+                
               }
 
           
         }
 
-        private bool CheckIfDateIsPresentInDatabase(SqlConnection connection, DateTime date)
+        private bool IsDateIsPresentInDatabase(SqlConnection connection, DateTime date)
         {
             bool isPresent;
 
@@ -78,24 +96,27 @@ namespace BusinessInteligenceLabs
         private void InsertDate(SqlConnection connection, DateTime date)
         {
             var insertCommand = new SqlCommand(@"INSERT INTO Time (dayName, dayNumber, monthName, monthNumber, weekNumber, year, weekend, date, dayOfYear)
-                                                    VALUES (@dayName, @dayNumber, @monthName, @monthNumber, @weekNumber, @year, @weekend, @date, @dayOfYear)");
+                                                    VALUES (@dayName, @dayNumber, @monthName, @monthNumber, @weekNumber, @year, @weekend, @date, @dayOfYear)", connection);
 
             SqlParameter[] parameterList = 
             {
-                new SqlParameter("@dayName", date.DayOfWeek),
+                new SqlParameter("@dayName", date.ToString("dddd")),
                 new SqlParameter("@dayNumber", date.DayOfWeek),
-                new SqlParameter("@monthName", date.DayOfWeek),
-                new SqlParameter("@monthNumber", date.DayOfWeek),
-                new SqlParameter("@weekNumber", date.DayOfWeek),
-                new SqlParameter("@year", date.DayOfWeek),
+                new SqlParameter("@monthName", date.ToString("MMMM")),
+                new SqlParameter("@monthNumber", date.Month),
+                new SqlParameter("@weekNumber", (int)date.DayOfYear/7),
+                new SqlParameter("@year", date.Year),
                 new SqlParameter("@weekend", IsWeekend(date)),
                 new SqlParameter("@date", date),
                 new SqlParameter("@dayOfYear", date.DayOfYear)
             };
             insertCommand.Parameters.AddRange(parameterList);
 
+            using (SqlDataReader reader = insertCommand.ExecuteReader())
+            {
+                Console.WriteLine($"Records affected {reader.RecordsAffected}");
+            }
 
-            bool IsWeekend(DateTime dateTime) => dateTime.ToString("dddd") == "Sunday" || dateTime.ToString("dddd") == "Sunday";
         }
     }
 }
